c06fb5cc56dcd6d4320c169f116f21226000fe08
Richie Kirchofer <rgkirch@gmail.com>
2015-05-20 17:26:30 -0400

    have vector(init list)

--- vector.cpp
+++ vector.cpp
@@ -5,22 +5,33 @@ class Vector {
 	T* elem;
 	unsigned int sz;
 public:
-	Vector() : sz{0}, elem{nullptr} {}
-	explicit Vector(int size) : sz{size}, elem{new int[size]} {}
-	Vector(std::initializer_list<T> init) : sz{static_cast<unsigned int>(init.size())}
-	{
-		for( int index = 0; index < sz; ++index )
-		{
-			elem[index] = init[index];
-		}
-	}
-	T& operator[] (int index) {return elem[index];}
-	const T& operator[] (int index) const {return elem[index];}
-	int size() {return sz;}
-	int* begin() {return elem;}
-	int* end() {return elem+sz;}
+	Vector();
+	explicit Vector( int );
+	Vector( std::initializer_list<T> );
+	T& operator[] ( int index ) { return elem[index]; }
+	const T& operator[] ( int index ) const { return elem[index]; }
+	int size() { return sz; }
+	int* begin() { return elem; }
+	int* end() { return elem+sz; }
 	void hello();
 };
+
+template <typename T>
+Vector<T>::Vector() : sz{0}, elem{nullptr}
+{}
+
+template <typename T>
+Vector<T>::Vector( int size ) : sz{size}, elem{new T[size]}
+{}
+
+template <typename T>
+Vector<T>::Vector( std::initializer_list<T> init ) : sz{static_cast<unsigned int>( init.size() )}
+{
+	for( int index = 0; index < sz; ++index )
+	{
+		elem[index] = init[index];
+	}
+}
 
 template <typename T>
 void Vector<T>::hello()
